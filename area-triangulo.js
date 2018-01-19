cf216879414d224dd0538d8afdc3e9bb9c346b84
francves <francves1711@gmail.com>
2018-01-18 22:20:34 -0400

    agregando el area de un circulo

--- area-triangulo.js
+++ area-triangulo.js
@@ -6,3 +6,9 @@ const triangleArea = (base, height) => base * height / 2
 console.log(`El área de un triángulo de base
 ${base} y altura ${height} es: ${triangleArea(base, height)}` )
 
+/*Area círculo*/
+let radio = 5
+
+const circleArea = (radio) => Math.PI * Math.pow(radio,2)
+
+console.log(`El área de un círculo de radio ${radio} es ${circleArea(radio)}`)
