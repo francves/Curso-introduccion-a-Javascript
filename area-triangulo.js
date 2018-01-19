97555533b0325bbb40a21c6924d1e7b7ba45d66d
francves <francves1711@gmail.com>
2018-01-18 22:15:25 -0400

    arrow function

--- area-triangulo.js
+++ area-triangulo.js
@@ -1,9 +1,7 @@
 let base =  5
 let height = 7
 
-function triangleArea(base, height){
-	return base * height / 2
-}
+const triangleArea = (base, height) => base * height / 2
 
 console.log(`El área de un triángulo de base
 ${base} y altura ${height} es: ${triangleArea(base, height)}` )
